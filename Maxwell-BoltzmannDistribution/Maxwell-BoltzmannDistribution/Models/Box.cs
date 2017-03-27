bfd961bf49baf4ebf81e4227e4320ada4b8915d6
Winterhart <m.user.work@gmail.com>
2017-03-27 16:12:44 -0400

    Adding Structure for Simulation, Particule, Box

---
+++ Maxwell-BoltzmannDistribution/Maxwell-BoltzmannDistribution/Models/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxwell_BoltzmannDistribution.Models
+{
+    internal class Box
+    {
+        private double Width;
+        private double Height;
+        
+        Boolean ElasticCollisionWithSide = true;
+        /// <summary>
+        /// Size of the box is given in meters...
+        /// </summary>
+        /// <param name="sizeX"></param>
+        /// <param name="sizeY"></param>
+        internal Box(double sizeX, double sizeY)
+        {
+            this.Width = sizeX;
+            this.Height = sizeY; 
+        }
+
+
+    }
+}

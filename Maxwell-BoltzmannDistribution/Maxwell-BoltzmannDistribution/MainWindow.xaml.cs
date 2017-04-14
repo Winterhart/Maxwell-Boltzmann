8337e90b8aef5d76f5e537f04ea9cee2d80ebde3
Winterhart <m.user.work@gmail.com>
2017-04-14 19:54:11 -0400

    Updated FRAMEWORK, addition of Graph, New Front-End, Packaging Updated, Validation on Front-End Input, Field to input data

--- Maxwell-BoltzmannDistribution/Maxwell-BoltzmannDistribution/MainWindow.xaml.cs
+++ Maxwell-BoltzmannDistribution/Maxwell-BoltzmannDistribution/MainWindow.xaml.cs
@@ -31,30 +31,49 @@ namespace Maxwell_BoltzmannDistribution
 
         private void Start_Sim(object sender, RoutedEventArgs e)
         {
-            // Hard coded simulation for starting
-            Simulation_Constant.BOX_HEIGHT = 0.5;
-            Simulation_Constant.BOX_WIDTH = 0.5;
-            Simulation_Constant.INITIAL_SPEED = 460;
-            Simulation_Constant.NUMBER_OF_PARTICULE = 100;
-            Simulation_Constant.PARTICULE_RADIUS = 0.000000001;
-            Simulation_Constant.TIME_IN_SECONDS = 10;
-            Simulation_Constant.CURRENT_GRAVITY = 9.81;
-            Simulation_Constant.SPEED_LOSS_FACTOR = 0.0001;
+            String Status = "";
+            try
+            {
 
-            Simulation_Constant.FLAG_END_SIMULATION = false;
+                Status = "WRONG BOX HEIGHT";
+                Simulation_Constant.BOX_HEIGHT = Convert.ToDouble(BoxHeight.Text);
+                Status = "WRONG BOX WIDTH";
+                Simulation_Constant.BOX_WIDTH = Convert.ToDouble(BoxWidth.Text);
+                Status = "WRONG Initial Speed";
+                Simulation_Constant.INITIAL_SPEED = Convert.ToDouble(InitialSpeed.Text);
+                Status = "WRONG Number of Particule";
+                Simulation_Constant.NUMBER_OF_PARTICULE = Convert.ToInt32(NumberOfParticule.Text);
+                Status = "WRONG time of Experience (Must be an integer)";
+                Simulation_Constant.TIME_IN_SECONDS = Convert.ToInt32(Time.Text);
+                Status = "Wrong Speed Loss Factor";
+                Simulation_Constant.SPEED_LOSS_FACTOR = Convert.ToDouble(SpeedLoss.Text);
+                Status = "WRONG Gravity";
+                Simulation_Constant.CURRENT_GRAVITY = Convert.ToDouble(Gravity.Text);
+                Status = "WRONG Particule Radius";
+                Simulation_Constant.PARTICULE_RADIUS = Convert.ToDouble(ParticuleRadius.Text);
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("One of the input is not correctly inserted Status of the ERROR: " + Status);
+                return;
+            }
+                Simulation_Constant.FLAG_END_SIMULATION = false;
+
             try
             {
                 MC.StartSimulation();
             }
             catch (Exception ee)
             {
-                Console.WriteLine(ee.Message);
+                MessageBox.Show("ERROR: " + ee.Message);
             }
             while (!Simulation_Constant.FLAG_END_SIMULATION)
             {
                 Mouse.OverrideCursor = Cursors.Wait;
             }
             Mouse.OverrideCursor = null;
+
+            MessageBox.Show("...");
        
 
         }
